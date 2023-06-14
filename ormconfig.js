3712ff17ab99091c129b1f46720ac2d33202c95a
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-06-14 16:31:44 -0500

    :sparkles: configure typeorm in project

---
+++ ormconfig.js
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+module.exports = {
+  type: process.env.DB_TYPE,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  synchronize: true,
+};
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+const host = process.env.DB_HOST;
+const port = process.env.DB_PORT;
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+const database = process.env.DB_DATABASE;
+
+console.log(host, port, username, password, database);
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host,
+      port: Number(port),
+      username,
+      password,
+      database,
+      entities: [__dirname + '/entities/*.entity{.ts,.js}'],
+      // migrations: [__dirname + '/migrations/*{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
