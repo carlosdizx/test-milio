00286996b9210a65fe6f8540d4a7795d0de774f1
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-06-14 19:56:15 -0500

    :sparkles: add properties in response

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,11 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): object {
-    return { message: 'Hello World!', author: 'Carlos Ernesto Díaz Basante' };
+    return {
+      message: 'Hello World!',
+      author: 'Carlos Ernesto Díaz Basante',
+      github: 'https://github.com/carlosdizx',
+      linkedIn: 'https://www.linkedin.com/in/carlos-ernesto-diaz-basante',
+    };
   }
 }
