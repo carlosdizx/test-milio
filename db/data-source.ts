94bbadf3f4f5533a23be925810c03440409c284c
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-06-15 00:40:09 -0500

    :sparkles: create scripts for usage typeorm as migrate entities

--- db/data-source.ts
+++ db/data-source.ts
@@ -1,9 +1,15 @@
+import dotenv = require('dotenv');
 import { DataSource, DataSourceOptions } from 'typeorm';
+
+dotenv.config();
+
 const host = process.env.DB_HOST;
 const port = process.env.DB_PORT;
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 const database = process.env.DB_DATABASE;
+
+console.log(host, port, username, password, database);
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
   host,
