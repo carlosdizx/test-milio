69cf2d9d839c67333cd50d139ebbb9f4b2d6891c
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-06-14 20:07:53 -0500

    :sparkles: update dtos

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,13 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+import { Transform } from 'class-transformer';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @MinLength(8)
+  @Transform(({ value }) => value.trim()) // Opcional: eliminar espacios en blanco alrededor del password
+  password: string;
+}
--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsNotEmpty, MinLength } from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsNotEmpty()
+  @MinLength(8)
+  password: string;
+}
