5199526dea13d5a4b9f99f317fe04272ca8acd49
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-06-14 20:09:12 -0500

    :sparkles: update entity user

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,13 @@
-export class User {}
+import { Entity, PrimaryGeneratedColumn, BaseEntity, Column } from 'typeorm';
+
+@Entity({ name: 'users' })
+export default class User extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  readonly id: string;
+
+  @Column({ name: 'email', type: 'varchar', unique: true })
+  email: string;
+
+  @Column({ name: 'password', type: 'varchar' })
+  password: string;
+}
